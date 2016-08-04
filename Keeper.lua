e04532b9d5e09bffa2c81a411abf00a10837ceae
Sonaza <temu92@gmail.com>
2016-08-04 17:27:45 +0300

    Remove tradeskill result hook

--- Keeper.lua
+++ Keeper.lua
@@ -128,13 +128,13 @@ function addon:HookTips()
 		end
 	end);
 	
-	hooksecurefunc(GameTooltip, "SetRecipeResultItem", function(tooltip, recipeID)
-		local link = C_TradeSkillUI.GetRecipeItemLink(recipeID);
-		if(link) then
-			addon:AddTooltipInfo(tooltip, link);
-			tooltip:Show();
-		end
-	end);
+	-- hooksecurefunc(GameTooltip, "SetRecipeResultItem", function(tooltip, recipeID)
+	-- 	local link = C_TradeSkillUI.GetRecipeItemLink(recipeID);
+	-- 	if(link) then
+	-- 		addon:AddTooltipInfo(tooltip, link);
+	-- 		tooltip:Show();
+	-- 	end
+	-- end);
 	
 	hooksecurefunc(GameTooltip, "SetRecipeReagentItem", function(tooltip, recipeID, reagentIndex)
 		local link = C_TradeSkillUI.GetRecipeReagentItemLink(recipeID, reagentIndex);
